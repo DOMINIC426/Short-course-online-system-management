f901665e10af3ad486dfb02ec03bf3d5ef851cb8
= <=>
2026-08-27 09:54:02 +0300

    i have deleted student repo

--- backend/src/main/java/com/scms/repository/student/CourseIntakeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.scms.repository.student;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseIntakeRepository extends JpaRepository<CourseIntake, Long> {
-}
--- backend/src/main/java/com/scms/repository/student/StudentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.scms.repository.student;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}

99c6c437ada8f0986c67be99596c94a5a794c8d6
= <=>
2026-08-28 13:20:43 +0300

    now front end is active

--- backend/src/main/java/com/scms/config/ModelMapperConfig.java
+++ backend/src/main/java/com/scms/config/ModelMapperConfig.java
@@ -1,14 +1,19 @@
 package com.scms.config;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 
+
 public class ModelMapperConfig {
+
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
-}
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
+    }}

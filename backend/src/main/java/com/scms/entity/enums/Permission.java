d48c23335b1f12793df9fd1e2ec19daa9031b521
Samweli Margwe <samwelifaraay2001@gmail.com>
2026-08-29 18:40:38 +0300

    fixing errors

--- backend/src/main/java/com/scms/entity/enums/Permission.java
+++ backend/src/main/java/com/scms/entity/enums/Permission.java
@@ -1,41 +1,7 @@
-package com.scms.entity;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.experimental.SuperBuilder;
-
-@Entity
-@Table(
-        name = "permissions",
-        uniqueConstraints = {
-                @UniqueConstraint(
-                        name = "uk_permissions_name",
-                        columnNames = "name"
-                )
-        }
+@Column(
+        name = "permission_name",
+        nullable = false,
+        unique = true,
+        length = 100
 )
-@Getter
-@Setter
-@NoArgsConstructor
-@SuperBuilder
-public class Permission extends BaseEntity {
-
-    @Column(
-            name = "name",
-            nullable = false,
-            unique = true,
-            length = 100
-    )
-    private String name;
-
-    @Column(
-            name = "description",
-            columnDefinition = "TEXT"
-    )
-    private String description;
-}
+private String name;
